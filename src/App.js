7cf6ec0e40bf086eebe2d857fdf007e31200a02a
renatogm24 <renato.garay@pucp.pe>
2022-03-16 21:58:43 -0500

    pokemon-axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [pokemons, setpokemons] = useState([]);
+
+  const handleSubmit = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
+      const {
+        data: { results },
+      } = response;
+      setpokemons(results);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-1/2 my-5 mx-auto flex flex-col items-center">
+      <Button variant="contained" onClick={handleSubmit} className="w-36">
+        Fetch Pokemons
+      </Button>
+      <div className="w-full flex flex-col items-center my-5">
+        <h2>Pokemon List</h2>
+        <ul className="my-4">
+          {pokemons.length > 0
+            ? pokemons.map((pokemon, idx) => <li key={idx}>{pokemon.name}</li>)
+            : "Empty list"}
+        </ul>
+      </div>
     </div>
   );
 }
